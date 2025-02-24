e363085b458e6fe8a2d98f5ece4d5615e0c27336
Ganesh Keerthi <sganeshkeerthi@gmail.com>
2025-02-24 09:08:58 +0530

    Second commit

---
+++ src/test/java/com/suggi/HelloMavenTest.java
@@ -0,0 +1,14 @@
+package com.suggi;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloMavenTest {
+
+	@Test
+	void test() {
+		assertEquals(true, true);
+	}
+
+}
